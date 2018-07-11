b8e85d4422eb9b2e744211897f4758b2c3d646ef
bobloy <alboblexloy@gmail.com>
2018-07-11 12:38:31 -0400

    Initial commit

---
+++ polls/urls.py
@@ -0,0 +1,10 @@
+from django.urls import re_path
+
+from polls import views
+
+urlpatterns = [
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    re_path(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    re_path(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+]

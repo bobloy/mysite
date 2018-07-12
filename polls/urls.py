6c59a386723fd1f4716e38ddd5fc5b1f9f681fcb
bobloy <alboblexloy@gmail.com>
2018-07-12 08:27:05 -0400

    Tutorial 4

--- polls/urls.py
+++ polls/urls.py
@@ -1,10 +1,22 @@
-from django.urls import re_path
+from django.urls import re_path, path
 
 from polls import views
 
+# urlpatterns = [
+#     re_path(r'^$', views.index, name='index'),
+#     re_path(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+#     re_path(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+#     re_path(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+# ]
+
+app_name = 'polls'
 urlpatterns = [
-    re_path(r'^$', views.index, name='index'),
-    re_path(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    re_path(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-    re_path(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    # ex: /polls/
+    path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -9,7 +9,9 @@ from polls.models import Question, Choice
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
+    # template = loader.get_template('polls/index.html')
     return render(request, 'polls/index.html', context)
+    # return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
@@ -29,6 +31,7 @@ def vote(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': "You didn't select a choice.",
